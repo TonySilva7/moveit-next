f7ccbab4bf7950359d0c29fdef5c7419c1aa0e59
TonySilva7 <tony.deve7oper@gmail.com>
2021-04-11 11:55:49 -0300

    Challenge and profile components created

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/components/Profile.module.css"
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/77233531?v=4" alt="Tony Silva"/>
+            <div>
+                <strong>Tony Silva</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}
